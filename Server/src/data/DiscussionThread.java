9bda5bbf8bcd4dc785cafb035ff036a5783ae996
bkun31 <bilel.besseghieur@univ-tlse3.fr>
2021-12-25 16:13:44 +0100

    Restructuration des fichiers (separation Client/Serveur)

---
+++ Server/src/data/DiscussionThread.java
@@ -0,0 +1,35 @@
+package data;
+
+import java.util.Collection;
+import java.util.List;
+
+public class DiscussionThread {
+    private int threadId;
+    private String title;
+    private int unreadCount;
+
+    public DiscussionThread(int threadId, String title, int unreadMessage) {
+    }
+
+    public int getThreadId() {
+    }
+
+    public String getObject() {
+    }
+
+    public List getMessages() {
+    }
+
+    public int getUnreadMessage() {
+    }
+
+    public void addMessages(Collection messages) {
+    }
+
+    public int compareTo(DiscussionThread discussionThread) {
+    }
+
+    public boolean equals(Object object) {
+    }
+
+}
---
+++ Server/src/data/Group.java
@@ -0,0 +1,31 @@
+package data;
+
+import java.util.List;
+import java.util.Map;
+
+public class Group {
+    private String groupName;
+    private boolean service;
+
+    public Group(String groupName, boolean service, List<User> users) {
+    }
+
+    public String getGroupName() {
+    }
+
+    public Map getUsers() {
+    }
+
+    public boolean isService() {
+    }
+
+    public List<DiscussionThread> getThreads() {
+    }
+
+    public int compareTo(Group group) {
+    }
+
+    public boolean equals(Object object) {
+    }
+
+}
